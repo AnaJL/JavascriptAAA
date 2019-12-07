42f100cd5f5c536e037abc9a3105c183a96d2aff
AnaJu <56701827+AnaJL@users.noreply.github.com>
2019-12-06 21:32:53 -0300

    Update app.js

--- app.js
+++ app.js
@@ -56,71 +56,78 @@ for( const pokemon of poke){
 result += res 
 }
 pokedexa.innerHTML = result
-const text = document.querySelector('input[id = "filter-name"]')
-text.addEventListener('click', function() {
-    pokedexa.innerHTML = ''
-text.addEventListener('change', function() {
-for( pokemon of poke){
-    if (text.value == pokemon.name){
-        pokedexa.innerHTML =  `<div class="pokemon" data-name="${pokemon.name}" data-type="${pokemon.type[0]},${pokemon.type[1]}" tabindex="${pokemon.id}">
-        <figure class="pokemon-figure">
-          <img src="img/${pokemon.name}.png" alt="${pokemon.name}">
-        </figure>
-        <section class="pokemon-description">
-          <span class="pokemon-id">#00${pokemon.id}</span>
-          <h1 class="pokemon-name">${pokemon.name}</h1>
-          <div class="pokemon-types">
-            <span class="pokemon-type background-${pokemon.type[0]}">${pokemon.type[0]}</span>
-            <span class="pokemon-type background-${pokemon.type[1]}">${pokemon.type[1]}</span>
+text = document.querySelector('input[id = "filter-name"]')
+text.addEventListener('keyup', keyup, false)
+function keyup(key){
+    if (key.keyCode == "16"){
+      if (text.value == ''){
+        rest = result
+      }
+      else {
+    rest = ''
+    for( pokemon of poke){
+        if (text.value == pokemon.name){
+            rest =  `<div class="pokemon" data-name="${pokemon.name}" data-type="${pokemon.type[0]},${pokemon.type[1]}" tabindex="${pokemon.id}">
+            <figure class="pokemon-figure">
+              <img src="img/${pokemon.name}.png" alt="${pokemon.name}">
+            </figure>
+            <section class="pokemon-description">
+              <span class="pokemon-id">#00${pokemon.id}</span>
+              <h1 class="pokemon-name">${pokemon.name}</h1>
+              <div class="pokemon-types">
+                <span class="pokemon-type background-${pokemon.type[0]}">${pokemon.type[0]}</span>
+                <span class="pokemon-type background-${pokemon.type[1]}">${pokemon.type[1]}</span>
+              </div>
+            </section>
+            <section class="pokemon-stats">
+            <div class="stat-row">
+              <div>hp</div>
+              <div class="stat-bar">
+                <div class="stat-bar-bg" style="width: ${pokemon.stats.hp * 100 /250}%">${pokemon.stats.hp}</div>
+              </div>
+            </div>
+            <div class="stat-row">
+              <div>attack</div>
+              <div class="stat-bar">
+                <div class="stat-bar-bg" style="width: ${pokemon.stats.attack * 100 /250}%">${pokemon.stats.attack}</div>
+              </div>
+            </div>
+            <div class="stat-row">
+              <div>defense</div>
+              <div class="stat-bar">
+                <div class="stat-bar-bg" style="width: ${pokemon.stats.defense * 100 /250}%">${pokemon.stats.defense}</div>
+              </div>
+            </div>
+            <div class="stat-row">
+              <div>sp-atk</div>
+              <div class="stat-bar">
+                <div class="stat-bar-bg" style="width: ${pokemon.stats.spatk * 100 /250}%">${pokemon.stats.spatk}</div>
+              </div>
+            </div>
+            <div class="stat-row">
+              <div>sp-def</div>
+              <div class="stat-bar">
+                <div class="stat-bar-bg" style="width: ${pokemon.stats.spdef * 100 /250}%">${pokemon.stats.spdef}</div>
+              </div>
+            </div>
+            <div class="stat-row">
+              <div>speed</div>
+              <div class="stat-bar">
+                <div class="stat-bar-bg" style="width: ${pokemon.stats.speed * 100 /250}%">${pokemon.stats.speed}</div>
+              </div>
+            </div>
+          </section>
           </div>
-        </section>
-        <section class="pokemon-stats">
-        <div class="stat-row">
-          <div>hp</div>
-          <div class="stat-bar">
-            <div class="stat-bar-bg" style="width: ${pokemon.stats.hp * 100 /250}%">${pokemon.stats.hp}</div>
-          </div>
-        </div>
-        <div class="stat-row">
-          <div>attack</div>
-          <div class="stat-bar">
-            <div class="stat-bar-bg" style="width: ${pokemon.stats.attack * 100 /250}%">${pokemon.stats.attack}</div>
-          </div>
-        </div>
-        <div class="stat-row">
-          <div>defense</div>
-          <div class="stat-bar">
-            <div class="stat-bar-bg" style="width: ${pokemon.stats.defense * 100 /250}%">${pokemon.stats.defense}</div>
-          </div>
-        </div>
-        <div class="stat-row">
-          <div>sp-atk</div>
-          <div class="stat-bar">
-            <div class="stat-bar-bg" style="width: ${pokemon.stats.spatk * 100 /250}%">${pokemon.stats.spatk}</div>
-          </div>
-        </div>
-        <div class="stat-row">
-          <div>sp-def</div>
-          <div class="stat-bar">
-            <div class="stat-bar-bg" style="width: ${pokemon.stats.spdef * 100 /250}%">${pokemon.stats.spdef}</div>
-          </div>
-        </div>
-        <div class="stat-row">
-          <div>speed</div>
-          <div class="stat-bar">
-            <div class="stat-bar-bg" style="width: ${pokemon.stats.speed * 100 /250}%">${pokemon.stats.speed}</div>
-          </div>
-        </div>
-      </section>
-      </div>
-       `
+           `
     } 
- } 
-})
-})
+    }
+    }
+         pokedexa.innerHTML = rest
+    }
+    }
 
 
-restype = '<option id ="aq" value="All">All</option>'
+restype = '<option value="All">All</option>'
 optiontype = []
 tipo = document.querySelector('select[id = "filter-type"]')
 tipo.addEventListener('click', function() {
@@ -134,4 +141,70 @@ tipo.addEventListener('click', function() {
 }
 tipo.innerHTML = restype
 })
+resultl = ''
+
+tipo.addEventListener('change', function() {
+  for( pokemon of poke){
+    for(i in pokemon.type){
+      if (pokemon.type[i] == tipo.value){
+        res= `<div class="pokemon" data-name="${pokemon.name}" data-type="${pokemon.type[0]},${pokemon.type[1]}" tabindex="${pokemon.id}">
+     <figure class="pokemon-figure">
+       <img src="img/${pokemon.name}.png" alt="${pokemon.name}">
+     </figure>
+     <section class="pokemon-description">
+       <span class="pokemon-id">#00${pokemon.id}</span>
+       <h1 class="pokemon-name">${pokemon.name}</h1>
+       <div class="pokemon-types">
+         <span class="pokemon-type background-${pokemon.type[0]}">${pokemon.type[0]}</span>
+         <span class="pokemon-type background-${pokemon.type[1]}">${pokemon.type[1]}</span>
+       </div>
+     </section>
+     <section class="pokemon-stats">
+     <div class="stat-row">
+       <div>hp</div>
+       <div class="stat-bar">
+         <div class="stat-bar-bg" style="width: ${pokemon.stats.hp * 100 /250}%">${pokemon.stats.hp}</div>
+       </div>
+     </div>
+     <div class="stat-row">
+       <div>attack</div>
+       <div class="stat-bar">
+         <div class="stat-bar-bg" style="width: ${pokemon.stats.attack * 100 /250}%">${pokemon.stats.attack}</div>
+       </div>
+     </div>
+     <div class="stat-row">
+       <div>defense</div>
+       <div class="stat-bar">
+         <div class="stat-bar-bg" style="width: ${pokemon.stats.defense * 100 /250}%">${pokemon.stats.defense}</div>
+       </div>
+     </div>
+     <div class="stat-row">
+       <div>sp-atk</div>
+       <div class="stat-bar">
+         <div class="stat-bar-bg" style="width: ${pokemon.stats.spatk * 100 /250}%">${pokemon.stats.spatk}</div>
+       </div>
+     </div>
+     <div class="stat-row">
+       <div>sp-def</div>
+       <div class="stat-bar">
+         <div class="stat-bar-bg" style="width: ${pokemon.stats.spdef * 100 /250}%">${pokemon.stats.spdef}</div>
+       </div>
+     </div>
+     <div class="stat-row">
+       <div>speed</div>
+       <div class="stat-bar">
+         <div class="stat-bar-bg" style="width: ${pokemon.stats.speed * 100 /250}%">${pokemon.stats.speed}</div>
+       </div>
+     </div>
+   </section>
+   </div>
+    `
+    resultl += res
+
+      }
+    }
+  }
+  pokedexa.innerHTML = resultl
+})
+
 
